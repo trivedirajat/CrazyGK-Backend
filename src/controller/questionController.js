ee5d3e20ef2a757d3e8a4e612f244931f8372c99
Nirav-CodnestX <nirav@codnestx.com>
2024-08-31 23:24:06 +0530

    Crud api created for questions

--- src/controller/questionController.js
+++ src/controller/questionController.js
@@ -1,12 +1,11 @@
 var questionModel = require('../models/questions');
-
+const mongoose = require('mongoose');
 async function addQuestion(req, res) {
   try {
-    const { question, options, answers, marks } = req.body;
-    const questionResponse = questionModel.create({
+    const { question, options, marks } = req.body;
+    const questionResponse = await questionModel.create({
       question,
       options,
-      answers,
       marks,
     });
     if (questionResponse) {
@@ -30,14 +29,120 @@ async function addQuestion(req, res) {
   }
 }
 
-async function editQuestion(req, res) {
-  const questionResponse = questionModel.findOne({
-    question,
-    options,
-    answers,
-    marks,
-  });
-    console.log("")
+async function getAllQuestion(req, res) {
+  try {
+    let { id } = req.params;
+    if (id) {
+      console.log(
+        'new mongoose.Types.ObjectId(id),',
+        new mongoose.Types.ObjectId(id)
+      );
+      const questionResponse = await questionModel.findOne({
+        _id: new mongoose.Types.ObjectId(id),
+      });
+      console.log('questionResponse', questionResponse);
+      if (questionResponse) {
+        return res.status(200).send({
+          status: 200,
+          message: `Questions fetched Successfully.`,
+          data: questionResponse,
+        });
+      }
+      return res.status(400).send({
+        status: 400,
+        message: 'Failed to fetch Questions.',
+      });
+    }
+    let { page, limit } = req.query;
+    if (page <= 1) {
+      page = 0;
+    }
+    if (limit > 20) {
+      limit = 20;
+    }
+    const questionResponse = await questionModel
+      .find()
+      .limit(limit)
+      .skip(limit * page);
+    const questionCount = await questionModel.count();
+    if (questionResponse) {
+      return res.status(200).send({
+        status: 200,
+        message: `Questions fetched Successfully.`,
+        data: questionResponse,
+        count: questionCount,
+      });
+    } else {
+      return res.status(400).send({
+        status: 400,
+        message: 'Failed to fetch Questions.',
+      });
+    }
+  } catch (error) {
+    console.log('Error', error);
+    var responce = {
+      status: 501,
+      message: 'Internal Server Error',
+    };
+    return res.status(501).send(responce);
+  }
 }
 
-module.exports = { addQuestion, editQuestion };
+async function editQuestion(req, res) {
+  try {
+    const questionResponse = await questionModel.findOne({
+      _id: new ObjectId(id),
+    });
+    const { question, options, marks } = req.body;
+    console.log('questionResponse', questionResponse);
+    questionResponse.question = question;
+    questionResponse.options = options;
+    questionResponse.marks = marks;
+    const updatedQuestionsResponse = await questionResponse.save();
+    if (updatedQuestionsResponse) {
+      return res.status(200).send({
+        status: 200,
+        message: `Questions updated Successfully.`,
+        data: updatedQuestionsResponse,
+      });
+    } else {
+      return res.status(400).send({
+        status: 400,
+        message: 'Failed to update Questions.',
+      });
+    }
+  } catch (error) {
+    var responce = {
+      status: 501,
+      message: 'Internal Server Error',
+    };
+    return res.status(501).send(responce);
+  }
+}
+
+async function deleteQuestion(req, res) {
+  try {
+    const questionResponse = await questionModel.deleteOne();
+    console.log('questionResponse', questionResponse);
+    if (questionResponse?.deletedCount > 0) {
+      return res.status(200).send({
+        status: 200,
+        message: `Questions deleted Successfully.`,
+        data: questionResponse,
+      });
+    } else {
+      return res.status(400).send({
+        status: 400,
+        message: 'Failed to delete Questions.',
+      });
+    }
+  } catch (error) {
+    var responce = {
+      status: 501,
+      message: 'Internal Server Error',
+    };
+    return res.status(501).send(responce);
+  }
+}
+
+module.exports = { addQuestion, editQuestion, deleteQuestion, getAllQuestion };
--- src/routes/questionRoute.js
+++ src/routes/questionRoute.js
@@ -2,10 +2,18 @@ var express = require('express');
 var router = express.Router();
 var { verifyRole } = require('../middleware/tokenVerify');
 
-var { addQuestion, editQuestion } = require('../controller/questionController');
+var {
+  addQuestion,
+  editQuestion,
+  deleteQuestion,
+  getAllQuestion,
+} = require('../controller/questionController');
 
+router.get('/', verifyRole, getAllQuestion);
+router.get('/:id', verifyRole, getAllQuestion);
 router.post('/addQuestion', verifyRole, addQuestion);
-router.post('/editQuestion', verifyRole, editQuestion);
+router.put('/editQuestion/:id', verifyRole, editQuestion);
+router.delete('/deleteQuestion/:id', verifyRole, deleteQuestion);
 
 // router.get('/getPlan', getPlan);
 
