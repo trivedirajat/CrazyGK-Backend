6e93fad62d0c565c49b4d0d7edd96c60bc9e4aa0
jay patel <bhensdadiyajay7@gmail.com>
2024-09-06 23:12:15 +0530

    fix: editer related controller

--- src/routes/currentAffairsRoute.js
+++ src/routes/currentAffairsRoute.js
@@ -1,29 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const multer = require("multer");
 
-
-var {addCurrentAffairs, getCurrentAffairs, deleteCurrentAffairs, currentAffairsStatusUpdate} = require('../controller/currentAffairsController');
-
+var {
+  addCurrentAffairs,
+  getCurrentAffairs,
+  deleteCurrentAffairs,
+  currentAffairsStatusUpdate,
+  getAllCurrentAffairs,
+} = require("../controller/currentAffairsController");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets/currentAffairs/");
-    },
-  
-    filename: function (req, file, cb) {
-        const ext = file.originalname.split('.').pop();
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, "currentAffairs-" + uniqueSuffix + '.' + ext);
-    },
-  });
-  
-  const upload = multer({ storage: storage });
+  destination: function (req, file, cb) {
+    cb(null, "public/assets/currentAffairs/");
+  },
 
-router.post('/addCurrentAffairs', upload.fields([{ name: "image" }]), addCurrentAffairs);
-router.post('/getCurrentAffairs', getCurrentAffairs);
-router.post('/deleteCurrentAffairs', deleteCurrentAffairs);
-router.post('/currentAffairsStatusUpdate', currentAffairsStatusUpdate);
+  filename: function (req, file, cb) {
+    const ext = file.originalname.split(".").pop();
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, "currentAffairs-" + uniqueSuffix + "." + ext);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post(
+  "/addCurrentAffairs",
+  upload.fields([{ name: "image" }]),
+  addCurrentAffairs
+);
+router.post("/getCurrentAffairs", getCurrentAffairs);
+router.post("/getallCurrentAffairs", getAllCurrentAffairs);
+router.post("/deleteCurrentAffairs", deleteCurrentAffairs);
+router.post("/currentAffairsStatusUpdate", currentAffairsStatusUpdate);
 
 module.exports = router;
